55bcca4c49537495278aa49a83c06a132a0c22b7
geeeeceeee <grant.charleston@gmail.com>
2021-01-10 20:04:05 +0000

    Useful array code

--- src/uk/co/rosslyn/ted/ArrayResizeDemo.java
+++ src/uk/co/rosslyn/ted/ArrayResizeDemo.java
@@ -2,9 +2,11 @@ package uk.co.rosslyn.ted;
 
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class ArrayResizeDemo {
 
-        private static Scanner s = new Scanner(System.in);
+        private static Scanner s = new Scanner(in);
         private static int[] baseData = new int[10];
 
        /* public static void main(String[] args) {
@@ -28,11 +30,8 @@ public class ArrayResizeDemo {
 
         private static void printArray(int[] arr)
         {
-            /*for(int i = 0; i < arr.length; i++)
-                System.out.print(arr[i] + " ");
-            System.out.println(); */
-            for (int j : arr) System.out.print(j + " ");
-            System.out.println();
+            for (int j : arr) out.print(j + " ");
+            out.println();
         }
 
         private static void resizeArray()
@@ -40,7 +39,7 @@ public class ArrayResizeDemo {
             int[] original = baseData;
 
             baseData = new int[12];
-            System.arraycopy(original, 0, baseData, 0, original.length);
+            arraycopy(original, 0, baseData, 0, original.length);
         }
 
     }
